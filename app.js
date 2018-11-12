46e5171a3d7f65700bcf7c676d61b6a3c7be7214
Tony Min <minchunlin@gmail.com>
2018-11-11 22:53:20 -0500

    add

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+
+/*
 const os=require('os');
 var totalMemory=os.totalmem();
 var freeMemory=os.freemem();
@@ -5,3 +8,12 @@ var freeMemory=os.freemem();
 
 console.log(`Total memory: ${totalMemory}`);
 console.log(`Free memory: ${freeMemory}`);
+*/
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
